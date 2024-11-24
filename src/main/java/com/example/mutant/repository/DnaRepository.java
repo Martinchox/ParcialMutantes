53cbe0bae4da2cf0fdcaac7625638855e4ff53ba
Martinchox <maporollan@gmail.com>
2024-11-24 02:29:33 -0300

    repository

---
+++ src/main/java/com/example/mutant/repository/DnaRepository.java
@@ -0,0 +1,10 @@
+package com.example.mutant.repository;
+
+import com.example.mutant.model.Dna;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DnaRepository extends JpaRepository<Dna, Long> {
+    boolean existsBySequence(String sequence); // Verificar ADN
+}

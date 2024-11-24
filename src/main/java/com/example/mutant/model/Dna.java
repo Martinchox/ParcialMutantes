beb0679854b0f7e570bd3dbd7dfe8d8c231f8a10
Martinchox <maporollan@gmail.com>
2024-11-24 02:29:23 -0300

    model

---
+++ src/main/java/com/example/mutant/model/Dna.java
@@ -0,0 +1,19 @@
+package com.example.mutant.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Dna {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String sequence; 
+
+    @Column(nullable = false)
+    private boolean isMutant; 
+}
